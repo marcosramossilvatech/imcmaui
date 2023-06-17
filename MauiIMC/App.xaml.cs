8d19cd41efca5c7c700ba4fe35ae6d6d69e2ee4a
Marcos <mrsilvatdas@gmail.com>
2023-06-16 21:49:38 -0300

    first commit

---
+++ MauiIMC/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MauiIMC.Mvvm.Views;
+
+namespace MauiIMC;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//MainPage = new AppShell();
+		MainPage = new NavigationPage(new ImcView());
+	}
+}
---
+++ MauiIMC/obj/Debug/net6.0-android/android/src/crc64cc935425b43c2b5a/MainActivity.java
@@ -0,0 +1,46 @@
+package crc64cc935425b43c2b5a;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("MauiIMC.MainActivity, MauiIMC", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MauiIMC.MainActivity, MauiIMC", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MauiIMC.MainActivity, MauiIMC", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
